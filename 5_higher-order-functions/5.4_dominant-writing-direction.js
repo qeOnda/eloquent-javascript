a3db9db3ec74bcfcfebabc88a66189f18d3e33da
Shinsuke  May <s.seanmay@gmail.com>
2021-05-17 13:25:38 +0900

    5.4 with explanations

--- 5_higher-order-functions/5.4_dominant-writing-direction.js
+++ 5_higher-order-functions/5.4_dominant-writing-direction.js
@@ -1,18 +1,22 @@
+// Takes code and finds language object whose range includes given code
 function characterScript(code) {
   for (let script of SCRIPTS) {
     if (script.ranges.some(([from, to]) => {
       return code >= from && code < to;
     })) {
-      return script.ranges;
+      return script;
     }
   }
   return null;
 }
-  
-function countBy(items, groupName) {
+
+// Takes items and applies testToGroup test to each item
+// then creates an array of objects named 'counts' with
+// shape {*nameOfItem*: *numberOfOccurrences*}. Return counts 
+function countBy(items, testToGroup) {
   let counts = [];
   for (let item of items) {
-    let name = groupName(item);
+    let name = testToGroup(item);
     let known = counts.findIndex(c => c.name == name);
     if (known == -1) {
       counts.push({name, count: 1});
@@ -23,6 +27,11 @@ function countBy(items, groupName) {
   return counts;
 }
 
+// Apply countBy with test (testToGroup) of characterScript(code)
+// where code is the JS method codePointAt. Lastly, filter out from
+// arr of obj with countBy shape counts of "none" which capture spaces,
+// and special symbols such as [!, ?, ., /,] etc... Reduce returns script
+// direction name with the most counts.
 function dominantDirection(text) {
   let counted = countBy(text, char => {
     let script = characterScript(char.codePointAt(0));
@@ -33,4 +42,3 @@ function dominantDirection(text) {
 
   return counted.reduce((a, b) => a.count > b.count ? a : b).name;
 }
-  
